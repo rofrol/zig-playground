d81c062a8adcf61e48cfed68b0ac4824430b8f1d
Roman Frołow <rofrol@gmail.com>
2023-02-22 14:53:18 +0100

    returnValueFromSwitchBlocks.zig

---
+++ returnValueFromSwitchBlocks.zig
@@ -0,0 +1,24 @@
+//usr/bin/env zig run --enable-cache "$0" -- "$@" | tail -n +2; exit;
+// https://www.reddit.com/r/Zig/comments/107wh7a/running_a_zig_file_like_a_script/
+
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() !void {
+    const val = 1;
+    const switched = blk: {
+      switch(val) {
+        0 => {
+          break :blk "zero";
+        },
+        1 => {
+          break :blk "one";
+        },
+        else => {
+          break :blk "other";
+        },
+      }
+    };
+
+    print("{s}\n", .{switched});
+}
