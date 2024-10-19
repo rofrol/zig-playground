63d6334188337e045b64446a4a928927a52f5280
Roman Frołow <rofrol@gmail.com>
2024-10-19 21:30:02 +0200

    mod_and_rem.zig

---
+++ mod_and_rem.zig
@@ -0,0 +1,24 @@
+//usr/bin/env zig run --enable-cache "$0" -- "$@" | tail -n +2; exit;
+// https://www.reddit.com/r/Zig/comments/107wh7a/running_a_zig_file_like_a_script/
+
+// https://www.mathworks.com/matlabcentral/answers/403870-difference-between-mod-and-rem-functions
+const std = @import("std");
+const print = std.debug.print;
+const expect = std.testing.expect;
+pub fn main() void {
+    print("@mod(-5, 3): {}\n", .{@mod(-5, 3)});
+    print("@mod(5, 3): {}\n", .{@mod(5, 3)});
+    print("@mod(-4, 4): {}\n", .{@mod(-4, 4)});
+    print("@mod(-4.1, 4): {}\n", .{@mod(-4.1, 4)});
+    print("@mod(-6, 4): {}\n", .{@mod(-6, 4)});
+    print("@mod(-7, 4): {}\n", .{@mod(-7, 4)});
+    print("@rem(-5, 3): {}\n", .{@rem(-5, 3)});
+    print("@rem(5, 3): {}\n", .{@rem(5, 3)});
+}
+
+test {
+    try expect(@mod(-5, 3) == 1);
+    try expect(@mod(5, 3) == 2);
+    try expect(@rem(-5, 3) == -2);
+    try expect(@rem(5, 3) == 2);
+}
